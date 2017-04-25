d40c2ecab6d25a0aba2033bf75c79645d8310703
laboratorios <laboratorios@LABA4.unlmlab.edu.ar>
2017-04-25 17:55:13 -0300

    everything

--- src/MatrizMath.java
+++ src/MatrizMath.java
@@ -62,13 +62,26 @@ public class MatrizMath {
 		MatrizMath matRes = new MatrizMath();
 		for(int i = 0; i<this.fil;i++){
 			for(int j = 0; j<mat.col;j++){
+				matRes.matriz[i][j] = 0;
 				for(int k = 0; k<this.col; k++)
-				matRes.matriz[i][j] = this.matriz[i][k] - mat.matriz[k][j];
+				matRes.matriz[i][j] += this.matriz[i][k] - mat.matriz[k][j];
 			}
 		}
 		matRes.fil = this.fil;
 		matRes.col = this.col;
 		return matRes;
+	}
+	public VectorMath productoVec(VectorMath vec){
+		VectorMath vecRes = new VectorMath();
+		double[] vector = null;
+		for(int i = 0; i<this.fil;i++){
+			vector[i] = 0;
+			for(int j = 0; j<this.col; j++){
+				vector[i] += vec.getVector()[j] * this.matriz[j][i];
+			}
+		}
+		return vecRes;
+		
 	}
 	
 }
